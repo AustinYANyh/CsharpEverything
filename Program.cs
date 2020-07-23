88f6f212b3142e1b18bddd3f82d27a3a2d6e3a0e
LuzihanYYH <2823568349@qq.com>
2020-07-23 22:30:50 +0800

    主程序cs文件

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_myeverything
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            login login = new login();
+            login.ShowDialog();
+
+            if(login.DialogResult == DialogResult.OK)
+            {
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
